8411bd2f33b3c9ef1a40673d835943817b2bc03b
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-05-12 10:50:22 -0400

    changed the pom.xml settings

---
+++ src/test/java/com/checkmarx/ast/CxAuthTest.java
@@ -0,0 +1,20 @@
+package com.checkmarx.ast;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CxAuthTest {
+
+    @Test
+    public void cxScanShow() {
+    }
+
+    @Test
+    public void cxAstScanList() {
+    }
+
+    @Test
+    public void cxScanCreate() {
+    }
+}
