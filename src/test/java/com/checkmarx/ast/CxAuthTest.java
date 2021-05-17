8ab63b3f123bcf60c39850f8d467c4636355878a
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-05-17 14:52:34 -0400

    configuration changed to run mvn test

--- src/test/java/com/checkmarx/ast/CxAuthTest.java
+++ src/test/java/com/checkmarx/ast/CxAuthTest.java
@@ -12,26 +12,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.junit.Assert.*;
 
 public class CxAuthTest {
-    private Logger log = LoggerFactory.getLogger(CxAuthTest.class.getName());
     CxAuth auth = null;
     CxScanConfig config = new CxScanConfig();
     List<CxScan> scanList = new ArrayList<CxScan>();
     Map<CxParamType,String> params = new HashMap<>();
-    // String client_id = System.getenv("CX_CLIENT_ID");
-    // String secret = System.getenv("CX_CLIENT_SECRET");
-    // String apikey = System.getenv("CX_APIKEY");
-    // String baseuri = System.getenv("CX_BASE_URI");
     Map<String,String> environmentVariables = System.getenv();
-    
 
     @Before
-    public void init() {     
+    public void init() throws InterruptedException,IOException,URISyntaxException{     
     if(environmentVariables.containsKey("CX_CLIENT_ID")) {
         config.setClient_id(environmentVariables.get("CX_CLIENT_ID"));
     }
@@ -44,23 +36,18 @@ public class CxAuthTest {
     if(environmentVariables.containsKey("CX_BASE_URI")) {
         config.setBaseuri(environmentVariables.get("CX_BASE_URI"));
     }
+    if(environmentVariables.containsKey("PATH_TO_EXECUTABLE")) {
+        config.setPathToExecutable(environmentVariables.get("PATH_TO_EXECUTABLE"));
+    }
     params.put(CxParamType.PROJECT_NAME,"TestCaseWrapper");
     params.put(CxParamType.SCAN_TYPES,"sast");
     params.put(CxParamType.D,".");    
     params.put(CxParamType.FILTER,"*.java");
-        try {
-             auth = new CxAuth(config,log);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+        
     }
 
     @Test
-    public void cxScanShow() throws JsonProcessingException {
+    public void cxScanShow() throws InterruptedException, IOException, URISyntaxException {
         init();
         if(auth != null && scanList.size()>0) {
             for(int index=0; index < 5; index++) {
@@ -75,64 +62,35 @@ public class CxAuthTest {
     }
 
     @Test
-    public void cxAstScanList() {
+    public void cxAstScanList() throws IOException, InterruptedException, URISyntaxException {
         init();
         if(auth != null) {
-            try {
-                scanList = auth.cxAstScanList();
-                assertTrue(scanList.size()>0);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            scanList = auth.cxAstScanList();
+            assertTrue(scanList.size()>0);
+            
         }
     }
 
     @Test
-    public void cxScanCreationWrongPreset() {
+    public void cxScanCreationWrongPreset() throws InterruptedException, IOException, URISyntaxException {
         init();
         if(auth != null) {
+            params.put(CxParamType.SAST_PRESET_NAME,"Checkmarx Default Jay");
+            CxScan scanResult = auth.cxScanCreate(params);
+            assertTrue(auth.cxScanShow(scanResult.getID()).getStatus().equalsIgnoreCase("failed"));
             
-            try {
-                params.put(CxParamType.SAST_PRESET_NAME,"Checkmarx Default Jay");
-                CxScan scanResult = auth.cxScanCreate(params);
-                assertTrue(auth.cxScanShow(scanResult.getID()).getStatus().equalsIgnoreCase("failed"));
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
         }
     }
 
     @Test
-    public void cxScanCreationSuccess() {
+    public void cxScanCreationSuccess() throws InterruptedException, IOException, URISyntaxException {
         init();
-        if(auth != null) {
+        if(auth != null) {          
+            params.put(CxParamType.SAST_PRESET_NAME,"Checkmarx Default");
+            CxScan scanResult = auth.cxScanCreate(params);
+            assertTrue(auth.cxScanShow(scanResult.getID()).getStatus().equalsIgnoreCase("completed"));
             
-            try {
-                params.put(CxParamType.SAST_PRESET_NAME,"Checkmarx Default");
-                CxScan scanResult = auth.cxScanCreate(params);
-                assertTrue(auth.cxScanShow(scanResult.getID()).getStatus().equalsIgnoreCase("completed"));
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
         }
     }
-
-    @Test
-    public void cxScanCreationAuthenticationFailed() {
-        init();
-        if(auth != null) {
-            
-            try {
-               // params.put(CxParamType.,"Checkmarx Default");
-                CxScan scanResult = auth.cxScanCreate(params);
-                assertTrue(auth.cxScanShow(scanResult.getID()).getStatus().equalsIgnoreCase("completed"));
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     
 }
