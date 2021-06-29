5491dce995f36172d9a09134db15a1b29832e347
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-06-29 08:55:54 +0100

    clean up unit tests

--- src/test/java/com/checkmarx/ast/CxAuthTest.java
+++ src/test/java/com/checkmarx/ast/CxAuthTest.java
@@ -1,96 +1,97 @@
 package com.checkmarx.ast;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+@RunWith(JUnit4.class)
 public class CxAuthTest {
-    CxAuth auth = null;
-    private Logger log = LoggerFactory.getLogger(CxAuthTest.class.getName());
-    CxScanConfig config = new CxScanConfig();
-    List<CxScan> scanList = new ArrayList<>();
-    Map<CxParamType, String> params = new HashMap<>();
-    Map<String, String> environmentVariables = System.getenv();
+    private static final Logger log = LoggerFactory.getLogger(CxAuthTest.class.getName());
+
+    private static final int VALID_RETURN_CODE = 0;
+    private static final String FAILED = "failed";
+    private static final String COMPLETED = "completed";
+
+    private CxAuth auth;
 
     @Before
     public void init() throws IOException, URISyntaxException {
-        if (environmentVariables.containsKey("CX_CLIENT_ID")) {
-            config.setClientId(environmentVariables.get("CX_CLIENT_ID"));
-        }
-        if (environmentVariables.containsKey("CX_CLIENT_SECRET")) {
-            config.setClientSecret(environmentVariables.get("CX_CLIENT_SECRET"));
-        }
-        if (environmentVariables.containsKey("CX_APIKEY")) {
-            config.setApiKey(environmentVariables.get("CX_APIKEY"));
-        }
-        if (environmentVariables.containsKey("CX_BASE_URI")) {
-            config.setBaseUri(environmentVariables.get("CX_BASE_URI"));
-        }
-        if (environmentVariables.containsKey("CX_BASE_AUTH_URI")) {
-            config.setBaseAuthUri(environmentVariables.get("CX_BASE_AUTH_URI"));
-        }
-        if (environmentVariables.containsKey("CX_TENANT")) {
-            config.setTenant(environmentVariables.get("CX_TENANT"));
-        }
-        if (environmentVariables.containsKey("PATH_TO_EXECUTABLE")) {
-            config.setPathToExecutable(environmentVariables.get("PATH_TO_EXECUTABLE"));
-        }
+        log.info("Init test");
 
+        Map<String, String> environmentVariables = System.getenv();
+        CxScanConfig config = new CxScanConfig();
+        config.setClientId(environmentVariables.getOrDefault("CX_CLIENT_ID", null));
+        config.setClientSecret(environmentVariables.getOrDefault("CX_CLIENT_SECRET", null));
+        config.setApiKey(environmentVariables.getOrDefault("CX_APIKEY", null));
+        config.setBaseUri(environmentVariables.getOrDefault("CX_BASE_URI", null));
+        config.setBaseAuthUri(environmentVariables.getOrDefault("CX_BASE_AUTH_URI", null));
+        config.setTenant(environmentVariables.getOrDefault("CX_TENANT", null));
+        config.setPathToExecutable(environmentVariables.getOrDefault("PATH_TO_EXECUTABLE", null));
+
+        auth = new CxAuth(config, log);
+    }
+
+    @NotNull
+    private Map<CxParamType, String> createParams() {
+        Map<CxParamType, String> params = new HashMap<>();
         params.put(CxParamType.PROJECT_NAME, "TestCaseWrapper");
         params.put(CxParamType.SCAN_TYPES, "sast");
         params.put(CxParamType.S, ".");
         params.put(CxParamType.FILTER, "*.java");
-        auth = new CxAuth(config, log);
+
+        return params;
     }
 
     @Test
-    public void cxScanShow() throws InterruptedException, IOException, URISyntaxException {
-        init();
-        if (scanList == null || scanList.size() == 0) {
-            cxAstScanList();
-        }
-        if (scanList.size() > 0) {
-            assertTrue(scanList.get(0) instanceof CxScan);
-        }
+    public void cxScanShow() throws InterruptedException, IOException {
+        List<CxScan> scanList = auth.cxAstScanList();
+
+        assertTrue(scanList.get(0) instanceof CxScan);
     }
 
     @Test
-    public void cxAstAuthValidate() throws IOException, InterruptedException, URISyntaxException {
-        init();
+    public void cxAstAuthValidate() throws IOException, InterruptedException {
         Integer validate = auth.cxAuthValidate();
-        assertEquals(0, validate.intValue());
+
+        assertEquals(VALID_RETURN_CODE, validate.intValue());
     }
 
     @Test
-    public void cxAstScanList() throws IOException, InterruptedException, URISyntaxException {
-        init();
-        scanList = auth.cxAstScanList();
+    public void cxAstScanList() throws IOException, InterruptedException {
+        List<CxScan> scanList = auth.cxAstScanList();
+
         assertTrue(scanList.size() > 0);
     }
 
     @Test
-    public void cxScanCreationWrongPreset() throws InterruptedException, IOException, URISyntaxException {
-        init();
+    public void cxScanCreationWrongPreset() throws InterruptedException, IOException {
+        Map<CxParamType, String> params = createParams();
         params.put(CxParamType.SAST_PRESET_NAME, "Checkmarx Default Jay");
+
         CxScan scanResult = auth.cxScanCreate(params);
-        assertTrue(auth.cxScanShow(scanResult.getID()).getStatus().equalsIgnoreCase("failed"));
+
+        assertTrue(auth.cxScanShow(scanResult.getID()).getStatus().equalsIgnoreCase(FAILED));
     }
 
     @Test
-    public void cxScanCreationSuccess() throws InterruptedException, IOException, URISyntaxException {
-        init();
+    public void cxScanCreationSuccess() throws InterruptedException, IOException {
+        Map<CxParamType, String> params = createParams();
         params.put(CxParamType.SAST_PRESET_NAME, "Checkmarx Default");
+
         CxScan scanResult = auth.cxScanCreate(params);
-        assertTrue(auth.cxScanShow(scanResult.getID()).getStatus().equalsIgnoreCase("completed"));
+        assertTrue(auth.cxScanShow(scanResult.getID()).getStatus().equalsIgnoreCase(COMPLETED));
     }
 }
