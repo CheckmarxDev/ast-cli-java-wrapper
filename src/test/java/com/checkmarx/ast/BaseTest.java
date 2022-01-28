339e0aa8edaa0b0572d89d5817df984734c46637
Mithilesh Pawar <Mithilesh.Pawar@checkmarx.com>
2022-01-27 20:41:10 -0500

    * Updated exec-maven-plugin from 1.2.1 to 3.0.0 * Updated jackson-databind from 2.11.4 to 2.13.1 * Changed the name of the AST project for tests.

--- src/test/java/com/checkmarx/ast/BaseTest.java
+++ src/test/java/com/checkmarx/ast/BaseTest.java
@@ -53,12 +53,12 @@ public abstract class BaseTest {
 
     protected Map<String, String> commonParams() {
         Map<String, String> params = new HashMap<>();
-        params.put(CxConstants.PROJECT_NAME, "AST-CLI-Java-Wrapper-Tests");
+        params.put(CxConstants.PROJECT_NAME, "CLI-Java-Wrapper-Tests");
         params.put(CxConstants.SOURCE, ".");
         params.put(CxConstants.FILE_FILTER, "!test");
         params.put(CxConstants.BRANCH, "main");
         params.put(CxConstants.SAST_PRESET_NAME, "Checkmarx Default");
-        params.put(CxConstants.AGENT, "AST-CLI-Java-Wrapper");
+        params.put(CxConstants.AGENT, "CLI-Java-Wrapper");
         return params;
     }
 }
