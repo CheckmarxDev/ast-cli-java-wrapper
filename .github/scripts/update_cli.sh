26ad0a72c44538a0ed8b41317cad231bf9fe1c56
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2022-04-11 10:16:49 +0100

    Update update_cli.sh

--- .github/scripts/update_cli.sh
+++ .github/scripts/update_cli.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 release=$1
-filename_windows=ast-cli_${release}_windows_x64.tar.gz
+filename_windows=ast-cli_${release}_windows_x64.zip
 filename_linux=ast-cli_${release}_linux_x64.tar.gz
-filename_darwin=ast-cli_${release}_darwin_amd64.tar.gz
+filename_darwin=ast-cli_${release}_darwin_x64.tar.gz
 
 #Windows
 echo "Updating windows binary"
 wget https://github.com/checkmarx/ast-cli/releases/download/${release}/${filename_windows}
-mkdir ./tmp/
-tar -xvzf ${filename_windows} -C ./tmp/
+unzip ${filename_windows} -d tmp
 mv ./tmp/cx.exe ./src/main/resources/cx.exe
 rm -r tmp
 rm ${filename_windows}
@@ -18,7 +17,7 @@ rm ${filename_windows}
 echo "Updating linux binary"
 wget https://github.com/checkmarx/ast-cli/releases/download/${release}/${filename_linux}
 mkdir ./tmp/
-tar -xvzf ${filename_linux} -C ./tmp/
+tar -xvzf  ${filename_linux} -C ./tmp/
 mv ./tmp/cx ./src/main/resources/cx-linux
 rm -r tmp
 rm ${filename_linux}
