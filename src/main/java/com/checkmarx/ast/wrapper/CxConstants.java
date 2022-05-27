80cf1247b435746248c395f39d3df339b18cfbb5
tiagobcx <tiago.baptista@checkmarx.com>
2022-05-27 11:07:11 +0100

    changing file flag

--- src/main/java/com/checkmarx/ast/wrapper/CxConstants.java
+++ src/main/java/com/checkmarx/ast/wrapper/CxConstants.java
@@ -49,7 +49,7 @@ public final class CxConstants {
     static final String CWE_ID = "--cwe-id";
     static final String LANGUAGE = "--language";
     static final String VULNERABILITY_TYPE = "--vulnerability-type";
-    static final String FILE_SOURCES = "--file-sources";
+    static final String FILE_SOURCES = "--file";
     static final String ADDITONAL_PARAMS = "--additional-params";
     static final String ENGINE = "--engine";
     static final String SUB_CMD_KICS_REALTIME = "kics-realtime";
