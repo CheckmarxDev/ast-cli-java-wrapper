aae8aa2863bb56454af26b867dc13d1c6c61f5f6
Jay Nanduri <jay.nanduri@checkmarx.com>
2022-08-09 14:22:52 -0400

    change auth test case during failure

--- src/test/java/com/checkmarx/ast/AuthTest.java
+++ src/test/java/com/checkmarx/ast/AuthTest.java
@@ -1,15 +1,17 @@
 package com.checkmarx.ast;
 
 import com.checkmarx.ast.wrapper.CxConfig;
+import com.checkmarx.ast.wrapper.CxConstants;
 import com.checkmarx.ast.wrapper.CxException;
 import com.checkmarx.ast.wrapper.CxWrapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Map;
 
 class AuthTest extends BaseTest {
-
     @Test
     void testAuthValidate() throws CxException, IOException, InterruptedException {
         Assertions.assertNotNull(wrapper.authValidate());
@@ -19,6 +21,7 @@ class AuthTest extends BaseTest {
     void testAuthFailure() {
         CxConfig cxConfig = getConfig();
         cxConfig.setBaseAuthUri("wrongAuth");
+        cxConfig.setApiKey("InvalidApiKey");
         Assertions.assertThrows(CxException.class, () -> new CxWrapper(cxConfig, getLogger()).authValidate());
     }
 }
