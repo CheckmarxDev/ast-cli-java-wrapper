621cbc8d985d3f97ca65ebaa39f05ea794451708
elchananarb <elchanan.arbiv@checkmarx.com>
2024-07-09 19:04:30 +0300

    up yml file

--- .github/scripts/update_cli.sh
+++ .github/scripts/update_cli.sh
@@ -1,40 +1,37 @@
 #!/bin/bash
-
-set -e  # Exit immediately if a command exits with a non-zero status
-set -o pipefail  # Pipelines return the status of the last command to exit with a non-zero status
-
 release=$1
 filename_windows=ast-cli_${release}_windows_x64.zip
 filename_linux=ast-cli_${release}_linux_x64.tar.gz
 filename_darwin=ast-cli_${release}_darwin_x64.tar.gz
 
-# Ensure the target directory exists
-mkdir -p src/main/resources
-
 # Windows
-echo "Updating Windows binary"
+echo "Updating windows binary"
 wget https://github.com/checkmarx/ast-cli/releases/download/${release}/${filename_windows}
 unzip ${filename_windows} -d tmp
-mv tmp/cx.exe src/main/resources/cx.exe
-rm -rf tmp
+mv ./tmp/cx.exe ./src/main/resources/cx.exe
+rm -r tmp
 rm ${filename_windows}
+git lfs track "./src/main/resources/cx.exe"
 
 # Linux
-echo "Updating Linux binary"
+echo "Updating linux binary"
 wget https://github.com/checkmarx/ast-cli/releases/download/${release}/${filename_linux}
-mkdir -p tmp
-tar -xvzf ${filename_linux} -C tmp
-mv tmp/cx src/main/resources/cx-linux
-rm -rf tmp
+mkdir ./tmp/
+tar -xvzf ${filename_linux} -C ./tmp/
+mv ./tmp/cx ./src/main/resources/cx-linux
+rm -r tmp
 rm ${filename_linux}
+git lfs track "./src/main/resources/cx-linux"
 
 # Darwin (Mac)
-echo "Updating Mac binary"
+echo "Updating mac binary"
 wget https://github.com/checkmarx/ast-cli/releases/download/${release}/${filename_darwin}
-mkdir -p tmp
-tar -xvzf ${filename_darwin} -C tmp
-mv tmp/cx src/main/resources/cx-mac
-rm -rf tmp
+mkdir ./tmp/
+tar -xvzf ${filename_darwin} -C ./tmp/
+mv ./tmp/cx ./src/main/resources/cx-mac
+rm -r tmp
 rm ${filename_darwin}
+git lfs track "./src/main/resources/cx-mac"
 
-echo "Update completed successfully"
+# Update .gitattributes
+git add .gitattributes
