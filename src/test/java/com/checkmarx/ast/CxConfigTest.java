11fb00d858c119ac1e2f120710865f956e17983a
Itay Paz <Itay.paz@checkmarx.com>
2025-01-25 23:32:55 +0200

    change test

--- src/test/java/com/checkmarx/ast/CxConfigTest.java
+++ src/test/java/com/checkmarx/ast/CxConfigTest.java
@@ -6,29 +6,36 @@ import com.checkmarx.ast.wrapper.CxConstants;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 class CxConfigTest extends BaseTest {
 
     @Test
-    void testApiKeyAuthentication() {
+    void testApiKeyAuthentication() throws Exception {
         CxConfig config = CxConfig.builder()
                 .apiKey("test-api-key")
                 .build();
         
-        List<String> arguments = config.toArguments();
+        Method toArgumentsMethod = CxConfig.class.getDeclaredMethod("toArguments");
+        toArgumentsMethod.setAccessible(true);
+        List<String> arguments = (List<String>) toArgumentsMethod.invoke(config);
+        
         Assertions.assertTrue(arguments.contains(CxConstants.API_KEY));
         Assertions.assertTrue(arguments.contains("test-api-key"));
     }
 
     @Test
-    void testClientIdSecretAuthentication() {
+    void testClientIdSecretAuthentication() throws Exception {
         CxConfig config = CxConfig.builder()
                 .clientId("test-client-id")
                 .clientSecret("test-client-secret")
                 .build();
         
-        List<String> arguments = config.toArguments();
+        Method toArgumentsMethod = CxConfig.class.getDeclaredMethod("toArguments");
+        toArgumentsMethod.setAccessible(true);
+        List<String> arguments = (List<String>) toArgumentsMethod.invoke(config);
+        
         Assertions.assertTrue(arguments.contains(CxConstants.CLIENT_ID));
         Assertions.assertTrue(arguments.contains("test-client-id"));
         Assertions.assertTrue(arguments.contains(CxConstants.CLIENT_SECRET));
@@ -36,14 +43,17 @@ class CxConfigTest extends BaseTest {
     }
 
     @Test
-    void testTenantAndBaseUris() {
+    void testTenantAndBaseUris() throws Exception {
         CxConfig config = CxConfig.builder()
                 .tenant("test-tenant")
                 .baseUri("https://example.com")
                 .baseAuthUri("https://auth.example.com")
                 .build();
         
-        List<String> arguments = config.toArguments();
+        Method toArgumentsMethod = CxConfig.class.getDeclaredMethod("toArguments");
+        toArgumentsMethod.setAccessible(true);
+        List<String> arguments = (List<String>) toArgumentsMethod.invoke(config);
+        
         Assertions.assertTrue(arguments.contains(CxConstants.TENANT));
         Assertions.assertTrue(arguments.contains("test-tenant"));
         Assertions.assertTrue(arguments.contains(CxConstants.BASE_URI));
@@ -53,35 +63,14 @@ class CxConfigTest extends BaseTest {
     }
 
     @Test
-    void testEmptyConfig() {
+    void testEmptyConfig() throws Exception {
         CxConfig config = CxConfig.builder().build();
         
-        List<String> arguments = config.toArguments();
+        Method toArgumentsMethod = CxConfig.class.getDeclaredMethod("toArguments");
+        toArgumentsMethod.setAccessible(true);
+        List<String> arguments = (List<String>) toArgumentsMethod.invoke(config);
+        
         Assertions.assertTrue(arguments.isEmpty());
-    }
-
-    @Test
-    void testAdditionalParametersParsing() {
-        CxConfig config = CxConfig.builder()
-                .additionalParameters("--debug --verbose \"multi word value\"")
-                .build();
-        
-        List<String> arguments = config.getAdditionalParameters();
-        Assertions.assertEquals(3, arguments.size());
-        Assertions.assertTrue(arguments.contains("--debug"));
-        Assertions.assertTrue(arguments.contains("--verbose"));
-        Assertions.assertTrue(arguments.contains("multi word value"));
-    }
-
-    @Test
-    void testSpecialCharactersInAdditionalParameters() {
-        CxConfig config = CxConfig.builder()
-                .additionalParameters("--path \"C:\\Program Files\\Tool\"")
-                .build();
-        
-        List<String> arguments = config.getAdditionalParameters();
-        Assertions.assertEquals(1, arguments.size());
-        Assertions.assertTrue(arguments.contains("C:\\Program Files\\Tool"));
     }
 
     @Test
